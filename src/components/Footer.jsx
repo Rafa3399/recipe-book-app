71bbd40c33a7b758b68f67a98ea948129b0bfa77
Rafa <rafaokc33@gmail.com>
2024-05-04 17:35:53 +0200

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,18 @@
 import "./Footer.css"
+import github from "../assets/github.png"
+import linkedin from "../assets/linkedin.png"
 
 function Footer (){
   return(
-    <div>
-      c
-    </div>
+    <footer>
+      <div id="footer-images">
+        <hr className="line"></hr>
+        <img src={github} className="github" alt="Github" />
+        <img src={linkedin} className="linkedin" alt="Linkedin" />
+        <hr className="line"></hr>
+      </div>
+      <p>By Rafael Hernandez Soler</p>
+    </footer>
   )
 }
 
